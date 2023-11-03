d6328e381af4f648b2927a5366894767b885c417
Ketsia <dusengeketty1@gmail.com>
2023-11-02 21:52:42 -0500

    Added Validations to the fields

---
+++ RazorPagesRestaurant/Migrations/20231103025118_New_DataAnnotations.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesRestaurant.Migrations
+{
+    /// <inheritdoc />
+    public partial class New_DataAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Restaurant",
+                type: "TEXT",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Restaurant",
+                type: "TEXT",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Restaurant",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Restaurant",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 80);
+        }
+    }
+}
--- RazorPagesRestaurant/Models/Restaurant.cs
+++ RazorPagesRestaurant/Models/Restaurant.cs
@@ -7,9 +7,17 @@ namespace RazorPagesRestaurant.Models;
 public class Restaurant
 {
     public int Id { get; set; }
+    [StringLength(80, MinimumLength = 3)]
+    [Required]
     public string? Name { get; set; }
+    [StringLength(80, MinimumLength = 3)]
+    [Required]
     public string? Location{ get; set; }
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+    [StringLength(80, MinimumLength = 3)]
+    [Required]
     public string Order { get; set; } = string.Empty;
 }
